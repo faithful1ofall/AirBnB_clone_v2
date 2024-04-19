19cb966f667642264aa2bd9e82736153b2225a04
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2024-04-19 16:40:42 +0100

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,7 +9,6 @@ from models.state import State
 
 app = Flask(__name__)
 '''The Flask application instance.'''
-app.url_map.strict_slashes = False
 
 
 @app.route('/states_list', strict_slashes=False))
